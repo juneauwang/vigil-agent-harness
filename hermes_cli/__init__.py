7afd97931c44a17c84356b425139fa545106f1ab
juneauwang <juneauwang@github.com>
2026-08-19 21:15:58 +0800

    release: bump v0.1.22(同步自 release efcb001)

--- hermes_cli/__init__.py
+++ hermes_cli/__init__.py
@@ -17,8 +17,8 @@ Provides subcommands for:
 import os
 import sys
 
-__version__ = "0.1.21"
-__release_date__ = "2026.8.18"
+__version__ = "0.1.22"
+__release_date__ = "2026.8.19"
 
 
 def _ensure_utf8():
